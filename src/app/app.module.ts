cbcc88a0cc8b05cc08dc9ae95cb760c39074f266
OvcharenkoDaniil <danila-bras@mail.ru>
2022-09-28 00:11:30 +0300

    Added http get method for data, fixed list of items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FlightCardComponent } from './flight-card/flight-card.component';
-
+import { FlightCardComponent } from './flight/flight-card/flight-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FlightListComponent } from './flight/flight-list/flight-list.component';
+import {FlightService} from "./services/flight.service";
 @NgModule({
   declarations: [
     AppComponent,
-    FlightCardComponent
+    FlightCardComponent,
+    NavBarComponent,
+    FlightListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FlightService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
