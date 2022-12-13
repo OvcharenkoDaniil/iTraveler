f469e47d2c92af6fdc47e8dfa330a26b81fe98e6
OvcharenkoDaniil <danila-bras@mail.ru>
2022-12-13 18:20:57 +0300

    search-nav-bar data updated

--- src/app/search-nav-bar/search-nav-bar.component.ts
+++ src/app/search-nav-bar/search-nav-bar.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
 import axios from "axios";
-import {MatDialog} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {AuthorizationComponent} from "../authorization/authorization.component";
 import {Router} from "@angular/router";
 import {AlertifyService} from "../services/alertify.service";
 import {IUser} from "../model/user";
 import {AuthService} from "../services/auth.service";
+import {LogoutComponent} from "../dialog/logout/logout.component";
 
 @Component({
   selector: 'app-search-nav-bar',
@@ -19,6 +20,7 @@ export class SearchNavBarComponent implements OnInit {
   // @ts-ignore
   loggedinUser: string;
   user:IUser;
+
   constructor(private dialog: MatDialog,
               private router:Router,
               private alertify: AlertifyService,
@@ -47,26 +49,47 @@ export class SearchNavBarComponent implements OnInit {
     return this.user;
   }
 
-  async createUser(user: any) {
-    try {
-      await axios.post(this.BASE_URL + this.CREATE_USER, user);
-    } catch (error) {
-      console.error(error);
+  // async createUser(user: any) {
+  //   try {
+  //     await axios.post(this.BASE_URL + this.CREATE_USER, user);
+  //   } catch (error) {
+  //     console.error(error);
+  //
+  //   }
+  //
+  // }
 
-    }
+  // async Register() {
+  //   try {
+  //     const response = await axios.get(this.BASE_URL + this.GET_FLIGHTS);
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-  }
-
-  async Register() {
-    try {
-      const response = await axios.get(this.BASE_URL + this.GET_FLIGHTS);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  openDialog() {
+  openAuthDialog() {
     this.dialog.open(AuthorizationComponent);
   }
+  openDialog() {
+    this.dialog.open(LogoutComponent);
+  }
+
+  // openDialog(enterAnimationDuration: string, exitAnimationDuration: string, dialogText: string): void {
+  //
+  //   const dialogRef = this.dialog.open(LogoutComponent, {
+  //     data: dialogText,
+  //     width: '250px',
+  //     enterAnimationDuration,
+  //     exitAnimationDuration,
+  //   });
+  //
+  //
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
+
 }
+
+
